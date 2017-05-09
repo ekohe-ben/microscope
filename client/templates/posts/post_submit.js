ed0f620c1650d1e7e285176c9d3202932946b715
Ben Zheng <ben@ekohe.com>
2017-05-09 15:52:02 -0700

    Revert to 9-4

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -23,15 +23,15 @@ Template.postSubmit.events({
     var errors = validatePost(post);
     if (errors.title || errors.url)
       return Session.set('postSubmitErrors', errors);
-
+      
     Meteor.call('postInsert', post, function(error, result) {
       // display the error to the user and abort
       if (error)
-        return Errors.throw(error.reason);
+        return throwError(error.reason);
 
       // show this result but route anyway
       if (result.postExists)
-        Errors.throw('This link has already been posted');
+        throwError('This link has already been posted');
 
       Router.go('postPage', {_id: result._id});
     });
